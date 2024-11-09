c78ce4a38b8f8595d61f2a43b16a2756134285f6
awa <awaheartemoji@gmail.com>
2024-11-09 21:21:22 +0000

    ok maayb astro....,.

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import dynamicImport from "astro-dynamic-import";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [dynamicImport()],
+});
---
+++ public/app-root.js
@@ -0,0 +1,6 @@
+export const paths = {
+	home: "/ae",
+	about: "/about",
+	contact: "/contact",
+	notFound: "/404",
+};
